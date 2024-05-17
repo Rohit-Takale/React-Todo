ca54535cc1572f4584760b4af99505f275d6ff03
Rohit-Takale <rohittakale01@gmail.com>
2024-05-17 18:27:31 +0530

    handleChange function to view more and view less

--- src/pages/Mecical.jsx
+++ src/pages/Mecical.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
 import Input from "../components/Input";
+import Button from "../components/Button";
 
 const Mecical = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [suggestions, setSuggetions] = useState([]);
+  const [expandedItems, setExpandedItems] = useState({});
+  const [selectedForm, setSelectedForm] = useState('Tablet');
+  // const [selectedStrengths, setSelectedStrengths] = useState(item.salt_forms_json['Tablet']);
 
   const fetchData = async () => {
     try {
@@ -31,6 +35,19 @@ const Mecical = () => {
     }
   }, [suggestions]);
 
+  const toggleExpanded = (id) => {
+    setExpandedItems((prev) => ({
+      ...prev,
+      [id]: !prev[id],
+    }));
+  };
+
+
+
+  const handleFormChange = (form) => {
+    setSelectedForm(form);
+    console.log(selectedForm);
+  }
   return (
     <>
       <div className="p-5">
@@ -48,15 +65,81 @@ const Mecical = () => {
               value={"value"}
               placeholder={"Search the Term"}
             />
-        
           </div>
         </div>
 
-        <div className="grid grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 gap-4">
           {suggestions ? (
-            suggestions.map((item) => <div key={item.id}>
-                <div>{item.salt}
-                </div></div>)
+            suggestions.map((item) => (
+              <div key={item.id}>
+                <div>
+                  {" "}
+                  <strong>Salt: </strong> {item.salt}
+                </div>
+                <div>
+                  {/* {expandedItems[item.id]
+                    ? item.available_forms.join(",")
+                    : item.available_forms.slice(0, 3).join(", ") +
+                      (item.available_forms.length > 3 ? "..." : "")} */}
+
+                  {expandedItems[item.id] ? (
+                    <div>
+                      <strong>Form:</strong>
+                      {item.available_forms.map((form, index) => (
+                        <>
+                          {" "}
+                          <Button
+                            title={form}
+                            key={index}
+                            // className={form === selectedForm ? 'selected' : ''}
+                            style={{ backgroundColor: form === selectedForm ? 'red ' : 'green' }}
+
+                            onClick={() => handleFormChange(form)
+                            
+                            }
+                          />
+                        </>
+                      ))}
+                      {item.available_forms.length > 3 && (
+                        <Button
+                          onClick={() => toggleExpanded(item.id)}
+                          title={
+                            expandedItems[item.id] ? "View Less" : "View More"
+                          }
+                        />
+                      )}
+                    </div>
+                  ) : (
+                    <div>
+                      {" "}
+                      <strong>Form:</strong>
+                      {item.available_forms.slice(0, 3).map((form, index) => (
+                        <>
+                          {" "}
+                          <Button
+                            title={form}
+                            key={index}
+                            style={{ backgroundColor: form === selectedForm ? 'red' : 'green' }}
+                            // className={form === selectedForm ? 'bg-gray-400' : ''}
+                            onClick={() => handleFormChange(form)}
+                          />
+                        </>
+                      ))}
+                      {item.available_forms.length > 3 && (
+                        <Button
+                          onClick={() => toggleExpanded(item.id)}
+                          title={
+                            expandedItems[item.id] ? "View Less" : "View More"
+                          }
+                        />
+                      )}
+                    </div>
+                  )}
+                </div>
+
+                <div></div>
+              </div>
+            ))
           ) : (
             <div>There are no results</div>
           )}
